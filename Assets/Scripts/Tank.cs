2a3959ea41fa07a1bd399c0789749e67d78b7c46
teycheng <pagnavathtey@gmail.com>
2025-01-23 13:53:36 -0800

    got the tank ,wheel,turret working

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -2,14 +2,10 @@ using UnityEngine;
 
 public class Tank : MonoBehaviour
 {
-    private float speed = 5f;
+    private float speed = 2f;
     private Rigidbody rb;
     public float verticalInput;
 
-    public Transform frontLeftWheel;
-    public Transform frontRightWheel;
-    public Transform backLeftWheel;
-    public Transform backRightWheel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,16 +22,9 @@ public class Tank : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 movement = transform.forward * verticalInput * speed;
-        rb.linearVelocity =  new Vector3(movement.x, rb.linearVelocity.y, movement.z);
+        Vector3 movement = Vector3.forward * verticalInput * speed;
+        transform.Translate(movement);
     }
 
-    void wheelRotation()
-{
-    float wheelRotation = speed * Time.deltaTime * Input.GetAxis("Vertical");
-    
-    frontLeftWheel.Rotate(wheelRotation, 0f, 0f);
-    frontRightWheel.Rotate(wheelRotation, 0f, 0f);
-}
 
 }
---
+++ Assets/Scripts/TurretRotation.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TurretRotation : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+    }
+}
